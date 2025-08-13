1fcdd041822145ef66c065513e0ad90d7b64d659
mikoto2000 <mikoto2000@gmail.com>
2025-08-13 09:59:01 +0900

    Spring Initializr.

---
+++ src/main/java/dev/mikoto2000/encrypter/EncrypterApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.encrypter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncrypterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncrypterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mikoto2000/encrypter/EncrypterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.encrypter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncrypterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
